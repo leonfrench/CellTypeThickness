46affa693804776da5035609696c7c16f8341ba9
Leon French <leonfrench@gmail.com>
2017-03-15 11:17:02 -0400

    First commit, includes result GO tables.

---
+++ R/GetBrainSpanCorrelations.R
@@ -0,0 +1,75 @@
+library(tidyr)
+library(magrittr)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(reshape2)
+
+pathToBrainSpanExpression <- "/Users/lfrench/Downloads/Old Downloads/merge methods test/gene_array_matrix_csv/" #exon array data
+setwd("/Users/lfrench/Desktop/results/CellTypeThickness")
+
+consistentGenes <-read_tsv("./CellTypeLists/human/AllenHBA_DK_ExpressionMatrix_GenesAbove0.446Threshold.tsv")$Gene 
+length(consistentGenes)
+
+#load brainspan expression
+source("./R/LoadBrainSpanExpressionFunctions.R")
+
+#load expression (slow)
+expression <- loadBrainSpanExpression(pathToBrainSpanExpression, doRankTransform = FALSE)
+donorMetaData <- getExpressionColumnData(pathToBrainSpanExpression)
+
+genesOfInterest <- consistentGenes
+setdiff(genesOfInterest, rownames(expression))
+genesOfInterest <- intersect(genesOfInterest, rownames(expression))
+samplesToPlot <-  getExpressionColumnData(pathToBrainSpanExpression) 
+
+#filter regions to remove early non sampled regions
+counts <- dplyr::summarise(group_by(donorMetaData, structure_name), n = n())
+regionsWithFewSamples <- subset(counts, n < 7)$structure_name
+samplesToPlot <- subset(donorMetaData, !(structure_name %in% regionsWithFewSamples))
+
+#match Anglitta's filter
+samplesToPlot <- subset(samplesToPlot, AgeInMonths >= 13*12 & AgeInMonths <= 40*12)
+
+expression <- expression[,rownames(samplesToPlot)]
+
+
+brainSpanExpression <- melt(as.matrix(expression))
+colnames(brainSpanExpression) <- c("GeneSymbol", "regionAndDonor", "value")
+brainSpanExpression$regionAndDonor <- as.character(brainSpanExpression$regionAndDonor)
+brainSpanExpression$region <- donorMetaData[brainSpanExpression$regionAndDonor,"structure_name"]
+brainSpanExpression$donor <- donorMetaData[brainSpanExpression$regionAndDonor,"donor_name"]
+
+#filter for 11 regions and average
+mapping <- read_csv("./data/RegionMapping.csv")
+
+brainSpanExpression <- tbl_df(brainSpanExpression)
+brainSpanExpression %<>% filter(region %in% mapping$`BrainSpan atlas`)
+#filter for 8162 genes
+brainSpanExpression %<>% filter(GeneSymbol %in% genesOfInterest)
+
+brainSpanExpression %<>% group_by(region, GeneSymbol) %>% summarise(median = median(value))
+
+#load Allen HBA data from freesurfer report
+freeSurferExp <-read_tsv("./CellTypeLists/human/AllenHBA_DK_ExpressionMatrix_GenesAbove0.446Threshold.tsv")
+colsToUse <- c("Gene", colnames(freeSurferExp)[colnames(freeSurferExp) %in% mapping$FreesurferShortName])
+freeSurferExp <- freeSurferExp[,colsToUse]
+freeSurferExp %<>% gather("region", "expression", -Gene)
+
+freeSurferExp <- inner_join(freeSurferExp, mapping, by=c("region"="FreesurferShortName")) 
+freeSurferExp %<>% group_by(Gene, `BrainSpan atlas`) %<>% summarise(median = median(expression)) #average into the brainspan regions
+
+#inner join the two datasets
+combinedExp <- inner_join(brainSpanExpression, freeSurferExp, by=c("GeneSymbol"="Gene", "region"="BrainSpan atlas"), suffix=c(".brainSpan",".AHBA")) 
+
+correlations <- combinedExp %>% group_by(GeneSymbol) %>% summarise(correlation = cor(median.brainSpan, median.AHBA), pvalue = cor.test(median.brainSpan, median.AHBA, alternative = "greater")$p.value)
+correlations %>% arrange(pvalue)
+write.csv(correlations, "./data/BrainSpanCorrelations.csv", row.names = F, quote=F)
+
+#test with nr3c1
+nr3c1 <- combinedExp %>% filter(GeneSymbol == "NR3C1")
+plot(nr3c1$median.AHBA, nr3c1$median.brainSpan)
+ggplot(nr3c1, aes(x=median.AHBA, y = median.brainSpan)) + geom_point() + geom_smooth(method="lm", se = F) + theme_bw()
+
+cor.test(nr3c1$median.AHBA, nr3c1$median.brainSpan)
+cor.test(nr3c1$median.AHBA, nr3c1$median.brainSpan,m='s')
---
+++ R/GetGOGroupsFilteredHumanLists.R
@@ -0,0 +1,81 @@
+library(magrittr)
+library(GO.db)
+library(AnnotationDbi)
+library(annotate)
+library(xlsx)
+library(dplyr)
+library(readr)
+
+library(homologene) #install via install_github('oganm/homologene')
+library(org.Hs.eg.db)
+library(AnnotationDbi)
+library(annotate)
+library(GO.db)
+library(tmod)
+
+
+setwd("/Users/lfrench/Desktop/results/CellTypeThickness/CellTypeLists")
+
+sheetNames <- c("Astrocyte","Endothelial","Ependymal","Microglia","Mural","CA1.Pyramidal","Interneuron","S1.Pyramidal","Oligodendrocyte")
+
+consistentGenes <-read_tsv("./human/BrainSpanCorrelations_p_less_than_0.05.tsv")$GeneSymbol  #genes with freesurfer/HBA to BrainSpan consistency
+excelFileName <- "./human/SupplementaryTable1xlsx.xlsx" #Zeisel genes with freesurfer consistency
+
+length(consistentGenes)
+
+#load gene ontology
+if (exists("geneSetsGO") && length(geneSetsGO$MODULES2GENES) > 1000 ) { #assume it's already loaded - needs a fix to see if the variable is declared
+} else {
+  go_object <- as.list(org.Hs.egGO2ALLEGS)
+  
+  symbolsInGO <- getSYMBOL(unique(unlist(go_object)), data='org.Hs.eg')
+  
+  #build GO sets for tmod -slow
+  tmodNames <- data.frame()
+  modules2genes <- list()
+  goGroupName <- names(go_object)[1]
+
+  goCount <- length(go_object)
+  count <- 1
+  for(goGroupName in names(go_object)) {
+    if (count %% 1000 == 0) print(paste(count, "of", goCount))
+    count <- count + 1
+    
+    goGroup <- go_object[goGroupName]
+    geneIDs <- unique(unlist(goGroup, use.names=F))  #discard evidence codes
+    genesymbols <- unique(getSYMBOL(geneIDs, data='org.Hs.eg'))
+    
+    genesymbols <- intersect(genesymbols, consistentGenes)
+    if (!(length(genesymbols) > 5 & length(genesymbols) < 200)) next();
+    if (Ontology(goGroupName) == "CC") next();
+    
+    modules2genes[goGroupName] <- list(genesymbols)
+    
+    tmodNames <- rbind(tmodNames, data.frame(ID=goGroupName, Title = Term(goGroupName)))
+  }
+  geneSetsGO <- makeTmod(modules = tmodNames, modules2genes = modules2genes)
+}
+
+
+for (sheetName in sheetNames) {
+  cellSheet <- tbl_df(read.xlsx(excelFileName,sheetName = sheetName, stringsAsFactors=F))
+  cellTypeGenes <- cellSheet$Gene
+  #test enrichment
+  result <- tbl_df(tmodHGtest(fg=cellTypeGenes, bg=consistentGenes, mset=geneSetsGO, qval = 1.01, filter = T))
+  result <- mutate(rowwise(result), ontology = Ontology(ID))
+  result %<>% rename("Overlapping genes" = b, "GO group size" = B) %<>% dplyr::select(-N)
+  #write enrichment
+  write.csv(result, paste0("./human/",sheetName, ".Zeisel.GO.results.csv"), row.names = F)
+}
+
+#merge the two neuron types
+CA1Excite <- read_csv("./human/CA1.Pyramidal.Zeisel.GO.results.csv") 
+hippExcite <- read_csv("./human/S1.Pyramidal.Zeisel.GO.results.csv") 
+
+#merge of the CA1 and S1 pyramidal neuron results
+merged <- inner_join(CA1Excite, hippExcite, by="ID", suffix = c(".CA1", ".S1"))
+merged %<>% select(-Title.S1, -`GO group size.S1`, -`ontology.S1`)
+merged %<>% mutate(percentOverlapCA1 = `Overlapping genes.CA1`/n.CA1)
+merged %<>% mutate(percentOverlapS1 = `Overlapping genes.S1`/n.S1)
+merged %<>% mutate(percentOverlapDiff = abs(percentOverlapCA1 - percentOverlapS1))
+write.csv(merged, paste0("/Users/lfrench/Desktop/results/ZeiselCortex/filteredLists/CA1.vrs.S1.Zeisel.GO.results.csv"), row.names = F)
